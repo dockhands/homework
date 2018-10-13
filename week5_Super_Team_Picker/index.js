ece13b3760b3ac5da0db5d75a69ceff11a14294a
Dan Hawkins <danhawkins4@gmail.com>
2018-10-12 17:07:36 -0700

    installed npm packages

--- week5_Super_Team_Picker/index.js
+++ week5_Super_Team_Picker/index.js
@@ -4,3 +4,9 @@ const app = express();
 app.get('/hello-world', function (req, res) {
     res.send('Hello, World!');
   });
+
+  const DOMAIN = 'localhost';
+const PORT = '5050';
+app.listen(PORT, DOMAIN, () => {
+  console.log(`🖥 Server listenning on http://${DOMAIN}:${PORT}`);
+});
